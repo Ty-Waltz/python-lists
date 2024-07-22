0dbd254cf272e29552c2e7763f5db3270de58fa3
Ty-Waltz <ty.waltz@yahoo.com>
2024-07-22 11:51:43 -0400

    Made the average calculator

--- list_transformation.py
+++ list_transformation.py
@@ -1,1 +1,8 @@
-print("hello")
+#reverse order of list
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort(reverse=True)
+
+sum_grades=sum(grades)
+avg_grades=sum_grades/len(grades)
+print(grades)
+print(f"The average grade was: {avg_grades} ") 
